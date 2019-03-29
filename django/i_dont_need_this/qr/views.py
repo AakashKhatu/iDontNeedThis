1738e51987af350581dab45fc40e708d4ee5358e
Aakash Khatu <aakashkhatu1998@gmail.com>
2019-03-29 16:35:03 +0530

    added urls and placeholder views

--- django/i_dont_need_this/qr/views.py
+++ django/i_dont_need_this/qr/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def scanned(request):
+    # pass
+    return HttpResponse("You scanned a qr code")
