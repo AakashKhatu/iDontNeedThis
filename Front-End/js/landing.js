26faf15e95c43b580cb53af3eed51b85f4ac5b66
Ajesh DS <ajeshd17@gmail.com>
2019-04-03 13:12:13 +0530

    Landing Page Login Form

--- Front-End/js/landing.js
+++ Front-End/js/landing.js
@@ -1,8 +1,18 @@
 document.addEventListener("DOMContentLoaded", e => {
-    console.log()
+    setTimeout(() => {
+        animateOnLogin();
+    }, 3000);
 });
 
 const animateOnLogin = e => {
     const container = document.querySelector(".auth-popup-container");
-    container.classList.add('active');
+    container.style.zIndex = 2;
+    container.style.opacity = 1;
+}
+
+const animateClose = e => {
+    const backdrop = document.querySelector('.auth-backdrop');
+    const container = document.querySelector(".auth-popup-container");
+    container.style.zIndex = -10;
+    container.style.opacity = 0;
 }
