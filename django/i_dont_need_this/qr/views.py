36b45bc960dc2c0a4d674f79f81ff9cffecc7178
aakashkhatu1998@gmail.com <aakashkhatu1998@gmail.com>
2019-04-05 03:49:55 +0530

    added closing statements

--- django/i_dont_need_this/qr/views.py
+++ django/i_dont_need_this/qr/views.py
@@ -6,9 +6,11 @@ from django.contrib.auth.decorators import login_required
 def scanned(request):
     rpi_ip = "192.168.0.10:5000"
     box_id = request.GET["id"]
-    box_state = request.GET
+    box_state = request.GET["open"]
+    # return HttpResponse(
+    #     "invalid arguments in url")
     print("qr code of box with id: {0} scanned".format(box_id))
-    url = "http://"+rpi_ip+"/?open="
+    url = "http://"+rpi_ip+"/?open="+box_state
     response = requests.request("GET", url)
     return HttpResponse(
         "You scanned a qr code and you should'nt be seeing this page")
