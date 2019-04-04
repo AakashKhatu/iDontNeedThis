f88969ef64356e3229716a1e1c9b1d19eb9853a6
aakashkhatu1998@gmail.com <aakashkhatu1998@gmail.com>
2019-04-04 21:43:03 +0530

    fixed import otp

---
+++ django/i_dont_need_this/web/otp.py
@@ -0,0 +1,16 @@
+import requests
+import random
+
+
+def send_otp(number):
+    url = "https://www.fast2sms.com/dev/bulk"
+
+    otp = random.randint(10000, 99999)
+    querystring = {"authorization": "ZM2aEdmsy3WHNI8xejK6kiJ4hCYrBuwfn9t5QSLpov0VRb7lcP0qHGS5fkgWtPNX2YhFrQy9JnBOZTD6",
+                   "sender_id": "FSTSMS", "language": "english", "route": "qt",
+                   "numbers": number, "message": "8528",
+                   "variables": "{AA}", "variables_values": otp}
+    headers = {
+        'cache-control': "no-cache"
+    }
+    return otp
