963cb861c2c7c17d189dfb7bd1b529ebf59e1f75
aakashkhatu1998@gmail.com <aakashkhatu1998@gmail.com>
2019-04-05 03:27:56 +0530

    updated qr view

--- django/i_dont_need_this/qr/views.py
+++ django/i_dont_need_this/qr/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
 import requests
-
 from django.contrib.auth.decorators import login_required
 
 
-@login_required
 def scanned(request):
-    # resp = requests.get("rpi url here")
-    return HttpResponse("You scanned a qr code")
+    rpi_ip = "192.168.0.10:5000"
+    box_id = request.GET["id"]
+    box_state = request.GET
+    print("qr code of box with id: {0} scanned".format(box_id))
+    url = "http://"+rpi_ip+"/?open="
+    response = requests.request("GET", url)
+    return HttpResponse(
+        "You scanned a qr code and you should'nt be seeing this page")
