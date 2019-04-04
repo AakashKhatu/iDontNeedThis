23321ba68f3071e61939f4bd6e65aba643376aef
sagar <sroyalgupta@gmail.com>
2019-04-04 11:40:11 +0530

    added frontend

--- django/i_dont_need_this/i_dont_need_this/urls.py
+++ django/i_dont_need_this/i_dont_need_this/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('web.urls')),
+    path('web/', include('web.urls')),
     path('scan/', include('qr.urls')),
 ]
