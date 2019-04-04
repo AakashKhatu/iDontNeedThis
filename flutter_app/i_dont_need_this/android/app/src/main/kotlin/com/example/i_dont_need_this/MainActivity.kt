4c2d01442da38a2182b50aa8bf9a414c88fad2ee
aakashkhatu1998@gmail.com <aakashkhatu1998@gmail.com>
2019-04-04 11:17:26 +0530

    fixed flutter qr code

---
+++ flutter_app/i_dont_need_this/android/app/src/main/kotlin/com/example/i_dont_need_this/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.i_dont_need_this
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
