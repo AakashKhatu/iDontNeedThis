b4e902f31aa4e68a5f4640f339dca3ad2ed04243
aakashkhatu1998@gmail.com <aakashkhatu1998@gmail.com>
2019-04-04 20:07:04 +0530

    added templates

--- django/i_dont_need_this/web/static/js/landing.js
+++ django/i_dont_need_this/web/static/js/landing.js
@@ -1,7 +1,34 @@
+window.onload = () => {
+    AOS.init();
+}
 const animateOnLogin = e => {
     const container = document.querySelector(e);
     container.style.opacity = 1;
     container.style.zIndex = 2;
+}
+
+const openPassword = () => {
+    const passwords = document.getElementsByClassName('password');
+    console.log(passwords);
+    for (let i = 0; i < passwords.length; i++) {
+        const pass = passwords[i];
+        pass.style.display = 'initial';
+        pass.style.opacity = 1;
+        console.log(pass)
+    }
+
+    document.getElementById('logsubmit').innerText = "SUBMIT";
+}
+
+
+const clickedNext = () => {
+    const phoneField = document.getElementById('phone');
+    const phone = phoneField.value;
+    const isValidP = /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/;
+    if (isValidP.test(phone)) {
+       openPassword(); 
+    }
+    console.log(isValidP.test(phone));
 }
 
 const animateClose = e => {
